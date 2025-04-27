03a96d90b3d9b571de4c2b4ff3e37b7e631c5b18
Gabriel Bazei <gabrielbazei@gmail.com>
2025-04-27 12:28:43 -0300

    Alterado o raspberry.py, agora retorna o mac como id e está completamente funcional

--- RaspBerry/raspberry.py
+++ RaspBerry/raspberry.py
@@ -1,48 +1,67 @@
-# Este de um worker para receber macs com base em um id específico 
+# Este de um worker para receber macs com base em um id especifico 
 # e executar o comando WakeOnLan em cima de um mac para ligar o computador
-import subprocess,requests,time
-#Troque aqui o ID do raspberry
-id = '1'
+import subprocess,requests,time,platform,uuid,os,sys
+
+# Funcao para obter o MAC Address da maquina
+# Dependendo do sistema operacional, utiliza metodos diferentes para obter o MAC Address
+def pega_mac():
+    system = platform.system()
+    if system == "Windows":
+        mac = uuid.getnode()
+        mac_address = ':'.join(['{:02x}'.format((mac >> ele) & 0xff) for ele in range(40, -1, -8)])
+        return mac_address
+    elif system == "Linux":
+        try:
+            result = subprocess.check_output("ip link", shell=True).decode()
+            for line in result.split("\n"):
+                if "link/ether" in line:
+                    parts = line.strip().split()
+                    return parts[1]
+        except Exception as e:
+            print(f"Erro ao obter MAC: {e}")
+            return None
+    return None
+# Funcao para verificar se o MAC Address e valido
+# O MAC Address deve ter o formato XX:XX:XX:XX:XX:XX, onde X e um digito hexadecimal
+def sanitizador(mac):
+    valido = 0
+    if len(mac) == 17: # Tamanho correto para MAC Address
+        temp = mac.upper() # Converte para maiusculas
+        for i, char in enumerate(temp): # Verifica cada caractere
+            if i in [2, 5, 8, 11, 14]: # Verifica se e um :
+                if char != ":":
+                    print(f"Esperado ':' na posicao {i}, mas encontrou '{char}'")
+                    valido += 1
+            else:
+                if char not in "0123456789ABCDEF": # Verifica se e um caractere valido
+                    print(f"Char invalido na posicao {i}: '{char}'")
+                    valido += 1
+    else:
+        # Se o tamanho nao for 17, imprime mensagem de erro
+        print("Tamanho invalido") 
+        print(len(mac))
+        valido += 1
+    # Se o MAC Address for valido, retorna True, caso contrario, retorna False
+    return valido == 0
+# Funcao para executar o comando WakeOnLan
+# Recebe o MAC Address como parametro e executa o comando WakeOnLan
+def wol(mac):
+    mac = mac.strip().replace('"', '') # Remove aspas e espacos desnecessarios
+    if sanitizador(mac): # Verifica se o MAC Address e valido
+        subprocess.run(["wakeonlan",mac]) # Executa o comando WakeOnLan com o MAC Address
+#MAIN
+id= pega_mac() # Obtem o MAC Address da maquina
+if id is None: # Se nao conseguiu obter o MAC Address, imprime mensagem de erro
+    print("Erro ao obter MAC Address")
+id = id.upper() # Converte o MAC Address para maiusculas
+print("ID da maquina", id)
 url="https://wakeonwan-bazei.azurewebsites.net/"
 url = url + "/id/" + str(id)
 tempo = 2 # Tempo de espera inicial
-
-# Função para verificar se o MAC Address é válido
-# O MAC Address deve ter o formato XX:XX:XX:XX:XX:XX, onde X é um dígito hexadecimal
-def sanitizador(mac):
-    valido = 0
-    mac = mac.strip().replace('"', '') # Remove aspas e espaços desnecessários
-    if len(mac) == 17: # Tamanho correto para MAC Address
-        temp = mac.upper() # Converte para maiúsculas
-        for i, char in enumerate(temp): # Verifica cada caractere
-            if i in [2, 5, 8, 11, 14]: # Verifica se é um :
-                if char != ":":
-                    print(f"Esperado ':' na posição {i}, mas encontrou '{char}'")
-                    valido += 1
-            else:
-                if char not in "0123456789ABCDEF": # Verifica se é um caractere válido
-                    print(f"Char inválido na posição {i}: '{char}'")
-                    valido += 1
-    else:
-        # Se o tamanho não for 17, imprime mensagem de erro
-        print("Tamanho inválido") 
-        print(len(mac))
-        valido += 1
-    
-    # Se o MAC Address for válido, retorna True, caso contrário, retorna False
-    return valido == 0
-
-# Função para executar o comando WakeOnLan
-# Recebe o MAC Address como parâmetro e executa o comando WakeOnLan
-def wol(mac):
-    if sanitizador(mac): # Verifica se o MAC Address é válido
-        print('WakeOnLan:',mac)
-        subprocess.run(["WakeOnLan",mac]) # Executa o comando WakeOnLan com o MAC Address
 while True:
     try:
-        response = requests.get(url) # Faz uma requisição GET para o servidor
+        response = requests.get(url) # Faz uma requisicao GET para o servidor
         tempo = 2 # Reseta o tempo de espera
-        print("connected, codigo:", response.status_code)
         if response.status_code == 201:
             wol(response.text)
     except:
@@ -50,5 +69,3 @@ while True:
         if tempo < 10: # Aumenta o tempo de espera em caso de erro
             tempo += 1
     time.sleep(tempo)
-
-
